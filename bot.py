50473eb534085709bdf08ce8f0b0bdbb44b880db
Huai-Xing <59436697+Huai-Xing@users.noreply.github.com>
2021-05-05 12:50:13 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,24 +7,26 @@ import logging
 #For heruko
 import os
 PORT = int(os.environ.get('PORT', 5000))
-#
 
+#For logging
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
 )
 
 logger = logging.getLogger(__name__)
 
+#Default parameters for Bot
 currentuser = 0
-callbacktype = "1"
+callbacktype = "1" 
 catetype = 10
-category = ["Budget","Food", "Transport", "Bills", "Shopping", "Fun", "Gifts", "Others"]
+category = ["Budget","Food", "Transport", "Bills", "Shopping", "Fun", "Gifts", "Others"] #Categories for spending
 resetnum = 10
 resetType = ["current month", "current year", "entire bot"]
 now = datetime.datetime.now()
 callyear = now.year
 budgetset = False
 
+#Checks if there is a need to create a new document for the database
 def checkData():
     global currentuser
     global budgetset
@@ -40,8 +42,10 @@ def checkData():
         db.create_new_month(curryear, currmonth, currentuser)
         budgetset = True
 
+#Command to initialise the bot. Send users inline keyboard options.
 def start(update, context):
     global currentuser
+    #Checks for user to determine which database to read and write data to
     if update.message.chat.username == id1 or update.message.chat.username == id2:
         if update.message.chat.username == id1:
             currentuser = 0
@@ -65,8 +69,10 @@ def start(update, context):
             "<b>Welcome to Saving So Baby Can Be A Tai Tai</b>!\n\nTo begin, please select an action." + budgettext, parse_mode='HTML', reply_markup=reply_markup)
         budgetset = False
 
+#Command to view all available bot actions        
 def actions(update, context):
     global currentuser
+    #Checks for user to determine which database to read and write data to
     if update.message.chat.username == id1 or update.message.chat.username == id2:
         if update.message.chat.username == id1:
             currentuser = 0
@@ -90,8 +96,10 @@ def actions(update, context):
             "What would you like to do now?" + budgettext, parse_mode='HTML', reply_markup=reply_markup)
         budgetset = False
 
+#Command to add spending for current month
 def add(update, context):
     global currentuser
+    #Checks for user to determine which database to read and write data to
     if update.message.chat.username == id1 or update.message.chat.username == id2:
         if update.message.chat.username == id1:
             currentuser = 0
@@ -114,8 +122,10 @@ def add(update, context):
         callbacktype = "2"
         budgetset = False
 
+#Command to reset user data by month or year     
 def reset(update, context):
     global currentuser
+    #Checks for user to determine which database to read and write data to
     if update.message.chat.username == id1 or update.message.chat.username == id2:
         if update.message.chat.username == id1:
             currentuser = 0
@@ -131,8 +141,10 @@ def reset(update, context):
             "What would you like to reset?", parse_mode='HTML', reply_markup=reply_markup)
         callbacktype = "4"
 
+#Command to view paired user data        
 def peek(update, context):
     global currentuser
+    #Checks for user to determine which database to read and write data to
     if update.message.chat.username == id1 or update.message.chat.username == id2:
         if update.message.chat.username == id1:
             currentuser = 0
@@ -147,6 +159,7 @@ def peek(update, context):
             "What would you like to peek?", parse_mode='HTML', reply_markup=reply_markup)
         callbacktype = "6"
 
+#callback data responds        
 def button(update, context):
     checkData()
     global callbacktype
@@ -158,14 +171,19 @@ def button(update, context):
     global callyear
     query = update.callback_query
     query.answer()
+    
+    #First call back from user - from main menu
     if callbacktype == "1":
+        #Data call back 1 - Get current month data
         if query.data[0] == "1":
             text = GetCurrentMonth(currentuser)
             query.edit_message_text(text, parse_mode='HTML')
+        #Data call back 2 - Set current month budget
         elif query.data[0] == "2":
             text = "Please send in your budget"
             query.edit_message_text(text, parse_mode='HTML')
             catetype = 0
+        #Data call back 3 - Add current month spending
         elif query.data[0] == "3":
             text = "Please select spending category:"
             keyboard = [[InlineKeyboardButton("Food", callback_data='1'), InlineKeyboardButton("Transport", callback_data='2')],
@@ -175,14 +193,17 @@ def button(update, context):
             reply_markup = InlineKeyboardMarkup(keyboard)
             query.edit_message_text(text, parse_mode='HTML', reply_markup=reply_markup)
             callbacktype = "2"
+        #Data call back 4 - View historical data
         else:
             text = "Please input year"
             query.edit_message_text(text, parse_mode='HTML')
             callbacktype = "3"
+    #Second call back from user - from spending menu
     elif callbacktype == "2":
         text = "Please send in amount spent for "
         catetype = int(query.data[0])
         query.edit_message_text(text + category[catetype], parse_mode='HTML')
+    #Fourth call back from user - from reset button
     elif callbacktype == "4":
         resetnum = int(query.data)
         text = "Are you sure you want to reset {x}? This action cannot be undone!".format(x = resetType[resetnum])
@@ -191,6 +212,7 @@ def button(update, context):
         reply_markup = InlineKeyboardMarkup(keyboard)
         query.edit_message_text(text, parse_mode='HTML', reply_markup=reply_markup)
         callbacktype = "5"
+    #Fifth call back from user - from reset menu
     elif callbacktype == "5":
         ans = int(query.data)
         now = datetime.datetime.now()
@@ -209,6 +231,7 @@ def button(update, context):
         query.edit_message_text(text, parse_mode='HTML')
         resetnum = 3
         callbacktype = "0"
+    #Sixth call back from user - from peek button
     elif callbacktype == "6":
         user = 0
         if currentuser == 0:
@@ -222,13 +245,16 @@ def button(update, context):
             text = GetData(curryear, 13, user)
             query.edit_message_text(text, parse_mode='HTML')
         callbacktype = "0"
+    #Invalid call back from user - return default call back
     else:
         text = GetData(callyear, int(query.data), currentuser)
         query.edit_message_text(text, parse_mode='HTML')
         callbacktype = "0"
 
+#Read in text input from user and to respond accordingly        
 def text(update, context):
     global currentuser
+    #Checks for user to determine which database to read and write data to
     if update.message.chat.username == id1 or update.message.chat.username == id2:
         if update.message.chat.username == id1:
             currentuser = 0
@@ -243,11 +269,13 @@ def text(update, context):
         curryear = currnow.year
         currmonth = currnow.month - 1
         value = update.message.text
+        #Update budget
         if callbacktype == "1" and catetype == 0:
             data = db.update(curryear, currmonth, catetype, float(value), currentuser)
             db.update(curryear, 12, catetype, float(value), currentuser)
             update.message.reply_text("Budget is now updated to $" + value, parse_mode='HTML')
             callbacktype = "0"
+        #Update spending
         elif callbacktype == "2":
             data = db.update(curryear, currmonth, catetype, float(value), currentuser)
             db.update(curryear, 12, catetype, float(value), currentuser)
@@ -255,7 +283,9 @@ def text(update, context):
             new = "{N:.2f}".format(N = data[2])
             update.message.reply_text(category[catetype] + " is updated from $" + old + " to $" + new, parse_mode='HTML')
             callbacktype = "0"
+        #View historical data
         elif callbacktype == "3":
+            #Invalid date
             if not db.check_data(int(value), 0, 0, currentuser)and int(value) > 2020:
                 text = " is in the future and there is no data yet. \nPlease enter another year, or type '/actions' to see available actions."
                 update.message.reply_text(str(value) + text, parse_mode='HTML')              
@@ -275,6 +305,7 @@ def text(update, context):
                     [InlineKeyboardButton("Nov", callback_data='10'), InlineKeyboardButton("Dec", callback_data='11')]]
                 reply_markup = InlineKeyboardMarkup(keyboard)
                 update.message.reply_text(text, parse_mode='HTML', reply_markup=reply_markup)              
+        #View available actions
         else:
             keyboard = [[InlineKeyboardButton("Current month status", callback_data='1')],
                 [InlineKeyboardButton("Add budget", callback_data='2')],
@@ -285,9 +316,11 @@ def text(update, context):
                 "Please select an action:", parse_mode='HTML', reply_markup=reply_markup)
             callbacktype = "1"
 
-token = os.environ['bot']
-id1 = os.environ['id1']
-id2 = os.environ['id2']
+token = '<Token for telegram bot>'
+id1 = '<Id of restricted user>'
+id2 = '<Id of restricted user>'
+
+#Retrieve and format all month data for the entire year for user
 def GetAllData(yearNum, monthCat, user):
     data = db.find(yearNum, 12, user)
     amtspent = data[1] + data[2] + data[3] + data[4] + data[5] + data[6] + data[7]
@@ -327,6 +360,7 @@ def GetAllData(yearNum, monthCat, user):
         fulloutput += curroutput
     return fulloutput
 
+#Retrieve and format specific month data for user
 def GetData(yearNum, month, user):
     yearNum = int(yearNum)
     month = int(month)
@@ -356,6 +390,7 @@ def GetData(yearNum, month, user):
         amtsaved = abs(amtsaved)
         return output.format(m = monthCat[month], y = yearNum, L1 = data[0], L2 = data[1], L3 = data[2], L4 = data[3], L5 = data[4], L6 = data[5], L7 = data[6], L8 = data[7], L9 = amtspent, L10 = amtsaved, P = pos)
 
+#Retrieve and format current month data for user
 def GetCurrentMonth(user):
     currnow = datetime.datetime.now()
     curryear = currnow.year
@@ -397,12 +432,14 @@ def main():
     updater = Updater(token, use_context=True)
 
     dispatcher = updater.dispatcher
-
+    
+    #Telegram bot commands
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("reset", reset))
     dispatcher.add_handler(CommandHandler("actions", actions))
     dispatcher.add_handler(CommandHandler("add", add))
     dispatcher.add_handler(CommandHandler("peek", peek))
+
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, text))
     dispatcher.add_handler(CallbackQueryHandler(button))
 
@@ -412,7 +449,7 @@ def main():
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
                           url_path=token)
-    updater.bot.setWebhook('https://floating-reaches-20199.herokuapp.com/' + token)
+    updater.bot.setWebhook('https://<appname>.herokuapp.com/' + token)
     
     # updater.start_polling()
     
